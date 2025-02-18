b3931e475ea7dc42c9d4e93182549a4c6ffb5ca6
TONY GAO <TONY.GAO@STU.OCSB.CA>
2025-02-18 17:49:01 +0000

    math stuff

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Tony G
+
+# Date: 2025-02-18
+
+# Does basic math stuff.\
+import math
+
+
+def main():
+    print("7-3={}".format(7 - 3))
+    print("5+5={}".format(5 + 5))
+    print("5*5={}".format(5 * 5))
+    print("12/6={}".format(12 / 6))
+    print("4^2={}".format(4 ^ 2))
+    print("math.sqrt(4)={}".format(math.sqrt(4)))
+
+
+if __name__ == "__main__":
+    main()
